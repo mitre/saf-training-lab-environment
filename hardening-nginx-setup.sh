53f3013575208d539223a3dc42a33a17b733916c
Emily Rodriguez <32680215+em-c-rod@users.noreply.github.com>
2023-02-03 14:35:00 -0500

    Update hardening-nginx-setup.sh

--- hardening-nginx-setup.sh
+++ hardening-nginx-setup.sh
@@ -6,6 +6,8 @@ docker exec nginx apt-get install -y python3.9
 
 docker exec nginx ls /usr/bin | grep python3.9
 
-git clone -b docker https://github.com/mitre/ansible-nginx-stigready-hardening.git
+git clone -b docker https://github.com/mitre/ansible-nginx-stigready-hardening.git || true
 
-cd ansible-nginx-stigready-hardening
+python3 -m pip install --user ansible
+
+cd /workspaces/saf-training-lab-environment/ansible-nginx-stigready-hardening
