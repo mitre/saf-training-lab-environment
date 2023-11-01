20d42e2ea72723ce70366a021a525f636276b0e0
CMH <christopher.m.hosmer@live.com>
2023-11-01 16:16:27 +0000

    Update verify-lab.sh
    * Provides messages to users about what they should expect in the verification step
    * Hides stderr so output is not so messy

--- verify-lab.sh
+++ verify-lab.sh
@@ -1,6 +1,18 @@
 #! /bin/bash
-npm update -g @mitre/saf
-echo "MITRE SAF version: " && saf --version 
-echo "InSpec version: " && inspec --version
-echo "Docker is running the container(s): " &&  docker ps
 
+echo '
+Verify the script returns the following information:
+* A "MITRE SAF" version number.
+* An "InSpec" version number.
+* Two running containers:
+  - nginx:latest
+  - redhat/ubi8
+'
+
+echo -e "Collecting verification information...\n"
+npm update -g @mitre/saf &> /dev/null
+versionSafe=$(saf --version 2> /dev/null)
+versionInspec=$(inspec --version 2> /dev/null)
+echo "* MITRE SAF Version: ${versionSafe}" 
+echo "* InSpec Version: ${versionInspec}"
+echo "* Docker is running the container(s):" &&  docker ps
