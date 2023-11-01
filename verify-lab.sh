9d9a94bf689dc0d353e97c374ae0dc29f87b5978
CMH <christopher.m.hosmer@live.com>
2023-11-01 17:37:32 +0000

    Update verify-lab.sh
    Added messages to stdout to help students better understand what they need to review to verify setup was successful.

--- verify-lab.sh
+++ verify-lab.sh
@@ -1,18 +1,34 @@
-#! /bin/bash
+#!/bin/bash
 
-echo '
+source ./prettyOutput.sh
+
+
+echo "${LINE_ASCII_CONSOLE}"
+
+echo -e "${VERB}
 Verify the script returns the following information:
-* A "MITRE SAF" version number.
-* An "InSpec" version number.
+* A \"MITRE SAF\" version number.
+* An \"InSpec\" version number.
 * Two running containers:
   - nginx:latest
   - redhat/ubi8
-'
+${RSET}"
 
-echo -e "Collecting verification information...\n"
+
+echo "${LINE_ASCII_CONSOLE}"
+
+echo -e "${VERB}Collecting verification information...${RSET}\n"
+
 npm update -g @mitre/saf &> /dev/null
 versionSafe=$(saf --version 2> /dev/null)
 versionInspec=$(inspec --version 2> /dev/null)
-echo "* MITRE SAF Version: ${versionSafe}" 
-echo "* InSpec Version: ${versionInspec}"
-echo "* Docker is running the container(s):" &&  docker ps
+containersRunning=$(docker ps)
+
+echo -e "${INFO}* MITRE SAF Version:${RSET} ${PASS}${versionSafe}${RSET}"
+echo -e "${INFO}* InSpec Version:${RSET} ${PASS}${versionInspec}${RSET}"
+echo -e "${INFO}* Docker is running the container(s):${RSET}\n${PASS}${containersRunning}${RSET}"
+
+
+echo "${LINE_ASCII_CONSOLE}"
+
+echo 'Verification complete.'
