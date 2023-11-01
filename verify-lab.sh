3bdbf90a8529e9263607c9bc4d13e235228fdecc
CMH <christopher.m.hosmer@live.com>
2023-11-01 19:04:50 +0000

    Update verify-lab.sh

--- verify-lab.sh
+++ verify-lab.sh
@@ -9,8 +9,8 @@ echo -e "${VERB}Verify the script returns the following information:
 * A \"MITRE SAF\" version number.
 * An \"InSpec\" version number.
 * Two running containers:
-  - nginx:latest
-  - redhat/ubi8
+  - nginx
+  - redhat
 ${RSET}"
 
 
@@ -19,13 +19,42 @@ echo -e "${LINE_ASCII_CONSOLE}\n"
 echo -e "${VERB}Collecting verification information...${RSET}\n"
 
 npm update -g @mitre/saf &> /dev/null
-versionSafe=$(saf --version 2> /dev/null)
+versionSaf=$(saf --version 2> /dev/null)
 versionInspec=$(inspec --version 2> /dev/null)
 containersRunning=$(docker ps)
+containersMissing=''
 
-echo -e "${HIGH}* MITRE SAF Version:${RSET} ${PASS}${versionSafe}${RSET}"
-echo -e "${HIGH}* InSpec Version:${RSET} ${PASS}${versionInspec}${RSET}"
-echo -e "${HIGH}* Docker is running the following container(s):${RSET}\n${PASS}${containersRunning}${RSET}"
+if [ -z "${versionSaf}" ]
+then
+    statusSaf="${FAIL}\"saf\" not installed${RSET}"
+else
+    statusSaf="${PASS}${versionSaf}${RSET}"
+fi
+
+if [ -z "${versionInspec}" ]
+then
+    statusInspec="${FAIL}\"inspec\" not installed${RSET}"
+else
+    statusInspec="${PASS}${versionInspec}${RSET}"
+fi
+
+if ! echo "${containersRunning}" | grep --silent --extended-regexp 'nginx'
+then
+   containersMissing="${containersMissing}The \"nginx\" container is missing.\n"
+fi
+if ! echo "${containersRunning}" | grep --silent --extended-regexp 'redhat8'
+then
+   containersMissing="${containersMissing}The \"redhat8\" container is missing.\n"
+fi
+
+echo -e "${HIGH}* MITRE SAF Version:${RSET} ${statusSaf}"
+echo -e "${HIGH}* InSpec Version:${RSET} ${statusInspec}"
+echo -e "${HIGH}* Docker is running the following container(s):${RSET}"
+echo "${containersRunning}" | \
+    GREP_COLORS='mt=0;32' \
+    grep --color=always --extended-regexp \
+    'nginx$|redhat8$|$'
+echo -e "${FAIL}${containersMissing}${RSET}"
 
 
 echo -e "${LINE_ASCII_CONSOLE}\n"
